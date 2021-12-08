6878c2f8f346919d6b6d0886bc64c6918c213b58
timkim <timk@adobe.com>
2021-12-08 18:31:53 +0000

    Deploying to gh-pages from  @ b5c28b918d394f2a1a72e31f630eb3a469253c31 🚀

--- redirections.js
+++ redirections.js
@@ -14,7 +14,7 @@
   let prefixPath =
     window.location.host.indexOf("localhost") === 0 ? "" : "events/docs/";
   let redirectPath = "";
-
+  let hostChange;
   if (
     (window.location.hash !== "" &&
       window.location.pathname === "/" &&
@@ -86,11 +86,7 @@
         redirectPath = prefixPath + "guides/using/cc-asset-event-setup/";
         break;
       case "#!adobedocs/adobeio-events/master/using/cloudmanager.md":
-        window.location.href =
-          "https://developer.adobe.com/experience-cloud/cloud-manager/guides/getting-started/create-event-integration/";
-        break;
-         case "https://www.adobe.io/events/docs/experience-cloud/cloud-manager/guides/getting-started/create-event-integration/":
-        window.location.href =
+        hostChange = 
           "https://developer.adobe.com/experience-cloud/cloud-manager/guides/getting-started/create-event-integration/";
         break;
       case "#!adobedocs/adobeio-events/master/using/experience-platform-event-setup.md":
@@ -123,7 +119,11 @@
         redirectPath = prefixPath + "guides/api/eventsingress_api/";
         break;
     }
-    window.location.href =
-      "http://" + window.location.host + "/" + redirectPath;
+    if(hostChange !== ''){
+        window.location.href = hostChange;
+    } else {
+        window.location.href ="http://" + window.location.host + "/" + redirectPath;
+    }
+
   }
 })();
